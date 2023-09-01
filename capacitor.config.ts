06cef2e448505b33c8268e44a9ba3ba3bd967368
jsnimda <7615255+jsnimda@users.noreply.github.com>
2023-09-01 23:26:48 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scroll-animate-101',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
